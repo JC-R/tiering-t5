c10c52dea633ca4a27840db7629003f059db2c7c
Juan Rodriguez <jcr365@nyu.edu>
2020-11-05 19:08:38 -0500

    bash scripts

--- scripts/get_embeddings.sh
+++ scripts/get_embeddings.sh
@@ -1,23 +1,28 @@
-# need environment variables:
-#  APPDIR=
-#  DATADIR
-#  OUTDIR
+#!/bin/bash
 
+# on my PC, GeForce RTX-1080ti 11Gb, this takes about 9Gb of gpu ram
+#   num_workers = 10
+#   encode_batch_size = 300
+#   segment batch size -> 3000
 
-7z e -so $DATADIR/$1.cleantext.json.7z | LC_ALL=C tr -dc '\0-\177' | python $APPDIR/generate-embeddings.py \
+# process #segment_batch_lines as the multiple of encode_batch and # workers
+let w=$3*$2
+
+APPDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+7z e -so cleantext/$1.cleantext.json.7z | LC_ALL=C tr -dc '\0-\177' | python $APPDIR/../src/generate-embeddings.py \
 --input - \
---max_size 500 \
 --model t5-base \
---batch_size 2000 \
+--max_doc_size 500 \
 --device cuda \
 --cleantext \
 --embeddings \
 --compressed \
 --verbose \
---output $OUTDIR/$1 \
---maxlines 500000
-$2
-
-# e.g.  $2 -> --lastdoc doc-title    // start AFTER this document
+--output $1 \
+--maxlines 500000 \
+--encode_batch_size $2 \
+--num_workers $3 \
+--segment_batch_size $w $4 $5
 
 
